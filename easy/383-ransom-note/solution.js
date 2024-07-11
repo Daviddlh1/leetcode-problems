353e39e682ffc1bd24556c760237a220d40ebc51
david De la Hoz <dricardelo@gmail.com>
2024-07-11 17:49:11 -0500

    other interesting solutions

--- easy/383-ransom-note/solution.js
+++ easy/383-ransom-note/solution.js
@@ -24,3 +24,39 @@ function canConstruct(ransomNote, magazine) {
   }
   return true;
 }
+
+function canConstructV2(ransomNote, magazine) {
+  const charsAmountInMagazine = new Map();
+  for (let char of magazine) {
+    if (!charsAmountInMagazine.has(char)) {
+      charsAmountInMagazine.set(char, 0);
+    }
+    const currentValue = charsAmountInMagazine.get(char);
+    charsAmountInMagazine.set(char, currentValue + 1);
+  }
+  for (let char of ransomNote) {
+    console.log(charsAmountInMagazine.get(char));
+    if (
+      charsAmountInMagazine.has(char) &&
+      charsAmountInMagazine.get(char) !== 0
+    ) {
+      const currentValue = charsAmountInMagazine.get(char);
+      charsAmountInMagazine.set(char, currentValue - 1);
+    } else {
+      return false;
+    }
+  }
+  return true;
+}
+function canConstructV3(ransomNote, magazine) {
+  for (let i = 0; i < ransomNote.length; i++) {
+    const char = ransomNote[i];
+    if (magazine.indexOf(char) === -1) {
+      return false;
+    }
+    magazine = magazine.replace(char, "");
+  }
+  return true
+}
+
+console.log(canConstructV3("a", "ab"));
