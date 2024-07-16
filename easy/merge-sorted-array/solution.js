445db031ee4758a402fd85ec0eed6d9b3fab70b4
david De la Hoz <dricardelo@gmail.com>
2024-07-16 17:13:53 -0500

    merge-sort

--- easy/merge-sorted-array/solution.js
+++ easy/merge-sorted-array/solution.js
@@ -1,3 +1,16 @@
 function merge(nums1, m, nums2, n) {
-    
+    for (let i = m; i < m + n; i++) {
+        nums1[i] = nums2[i - m]
+    }
+
+    nums1.sort((a, b) => a - b)
+    console.log(nums1)
+}
+
+merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3)
+
+function moveElementsToRight(arr, index) {
+    for (let i = arr.length - 1; i > index; i--) {
+        arr[i] = arr[i - 1]
+    }
 }
