60357dd67b05ef47bc0149cbfe2df9341e1afaeb
david De la Hoz <dricardelo@gmail.com>
2024-08-11 10:40:12 -0500

    final questions

--- easy/arrays-101-card/final-excercises/squares-of-a-sorted-array/solution.js
+++ easy/arrays-101-card/final-excercises/squares-of-a-sorted-array/solution.js
@@ -7,4 +7,18 @@ function sortedSquares(nums) {
     return squares.sort((a,b) => a - b)
 }
 
-sortedSquares([-4,-1,0,3,10])
+// /**
+//  * @param {number[]} nums
+//  * @return {number[]}
+//  */
+// function sortedSquares2(nums) {
+//     let left = 0
+//     let right = nums.length - 1
+//     while(left <= right) {
+//         if (Math.abs(nums[left]) > Math.abs(nums[right])) {
+
+//         }
+//     }
+// }
+
+// console.log(sortedSquares2([-4,-1,0,3,10]))
